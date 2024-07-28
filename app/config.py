e89331bbe45958f8812366bb8f5652129746c625
ramonfrombr <ramonfrombr@gmail.com>
2024-07-28 09:26:58 -0300

    Imported bootstrap.js and jquery.js. Created collapsible menu inside aside

--- app/config.py
+++ app/config.py
@@ -5,8 +5,10 @@ class EnglishConfig(Config):
     LANGUAGE='english',
     BACKGROUND_COLOR = '#a51c30'
     TITLE = "CS50x in English"
+    INTRO = "CS50’s Introduction to Computer Science"
 
 class SpanishConfig(Config):
     LANGUAGE='spanish',
-    BACKGROUND_COLOR = 'darkorange'
+    BACKGROUND_COLOR = 'orangered'
     TITLE = "CS50x en Español"
+    INTRO = "Curso de Introducción a la Ciencia de la Computación de la Universidad de Harvard"
